adf026ac6c295390cd6842b60e0535cf34e08edf
Rados123 <79587683+Rados123@users.noreply.github.com>
2022-05-27 01:18:13 +0200

    Selekcja z listy zrobiona, po za powiatami one są po macoszemu naprawiony błąd w JS

--- src/main/resources/static/home.js
+++ src/main/resources/static/home.js
@@ -107,13 +107,3 @@ $(document).ready(function(){
 
     })
 });
-let sidebar = document.querySelector(".sidebar");
-
-let top = localStorage.getItem("sidebar-scroll");
-if (top !== null) {
-    sidebar.scrollTop = parseInt(top, 10);
-}
-
-window.addEventListener("beforeunload", () => {
-    localStorage.setItem("sidebar-scroll", sidebar.scrollTop);
-});
